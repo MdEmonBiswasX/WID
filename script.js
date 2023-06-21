af9c967e6c12e085aa55588c4ba8a95a6315eb3d
MD Emon Biswas <63392254+MdEmonBiswasX@users.noreply.github.com>
2023-06-21 17:24:12 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,83 +1,63 @@
-function extractImages() {
-  const websiteLink = document.getElementById('website-link').value;
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Website Image Extractor</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.5.0/css/bootstrap.min.css">
+  <style>
+    .image-preview {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: center;
+      gap: 10px;
+      margin-top: 20px;
+    }
+    
+    .image-preview img {
+      width: 200px;
+      height: 200px;
+      object-fit: cover;
+      cursor: pointer;
+    }
+    
+    .selected {
+      border: 3px solid blue;
+    }
 
-  // Display status message
-  setStatusMessage(`Extracting images from ${websiteLink}...`);
+    .status-message {
+      margin-top: 20px;
+    }
+  </style>
+</head>
+<body>
+  <div class="container">
+    <h1 class="mt-5">Website Image Extractor</h1>
+  
+    <div class="row mt-4">
+      <div class="col-md-6">
+        <label for="website-link" class="form-label">Website Link:</label>
+        <input type="url" id="website-link" class="form-control" placeholder="Paste website link here" required>
+      </div>
+      <div class="col-md-6">
+        <button onclick="extractImages()" class="btn btn-primary">Extract Images</button>
+      </div>
+    </div>
+  
+    <h2 class="mt-5">Preview:</h2>
+    <div id="image-preview" class="image-preview"></div>
+  
+    <div class="row mt-4">
+      <div class="col-md-6">
+        <button onclick="downloadImages()" class="btn btn-success">Download Selected Images</button>
+      </div>
+      <div class="col-md-6">
+        <button onclick="selectAllImages()" class="btn btn-secondary me-2">Select All</button>
+        <button onclick="deselectAllImages()" class="btn btn-secondary">Deselect All</button>
+      </div>
+    </div>
 
-  // Simulating a delay to show the status message
-  setTimeout(() => {
-    // Simulated response from the server with image URLs
-    const imageUrls = ['image1.jpg', 'image2.jpg', 'image3.jpg'];
-
-    // Display status message
-    setStatusMessage(`Images extracted from ${websiteLink}.`);
-
-    displayImages(imageUrls);
-  }, 2000);
-}
-
-function displayImages(imageUrls) {
-  const imagePreview = document.getElementById('image-preview');
-  imagePreview.innerHTML = '';
-
-  for (let i = 0; i < imageUrls.length; i++) {
-    const imageUrl = imageUrls[i];
-
-    const img = document.createElement('img');
-    img.src = imageUrl;
-    img.addEventListener('click', function() {
-      toggleImageSelection(this);
-    });
-
-    imagePreview.appendChild(img);
-  }
-
-  // Display status message
-  setStatusMessage(`${imageUrls.length} images displayed.`);
-}
-
-function toggleImageSelection(image) {
-  image.classList.toggle('selected');
-}
-
-function downloadImages() {
-  const selectedImages = document.getElementsByClassName('selected');
-
-  if (selectedImages.length === 0) {
-    // Display status message
-    setStatusMessage('No images selected for download.');
-    return;
-  }
-
-  // Display status message
-  setStatusMessage(`Downloading ${selectedImages.length} selected image(s).`);
-
-  for (let i = 0; i < selectedImages.length; i++) {
-    const imageUrl = selectedImages[i].src;
-    const fileName = `image_${i + 1}.jpg`;
-
-    // Create a link element
-    const link = document.createElement('a');
-    link.href = imageUrl;
-    link.download = fileName;
-    link.target = '_blank';
-    link.style.display = 'none';
-
-    // Add the link to the document body
-    document.body.appendChild(link);
-
-    // Click the link element to trigger the download
-    link.click();
-
-    // Remove the link from the document body
-    document.body.removeChild(link);
-  }
-
-  // Display status message
-  setStatusMessage('Image download completed.');
-}
-
-function setStatusMessage(message) {
-  const statusBox = document.getElementById('status-box');
-  statusBox.textContent = message;
-}
+    <div id="status-box" class="status-message"></div>
+  </div>
+  
+  <script src="script.js"></script>
+</body>
+</html>
